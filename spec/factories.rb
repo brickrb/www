982c52038262c6da1ccb681924e84243776165cb
maclover7 <me@jonathanmoss.me>
2015-05-30 18:21:35 -0400

    Generated user model

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,12 @@
 FactoryGirl.define do
+
   factory :package do
     name { Faker::Name.first_name }
   end
 
+  factory :user do
+    email { Faker::Internet.email }
+    password "foobarfoobar"
+    password_confirmation "foobarfoobar"
+  end
 end
