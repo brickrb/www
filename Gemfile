dc95812bc4dcf596b1568b40360a3429f0ee5e61
maclover7 <me@jonathanmoss.me>
2015-05-30 17:21:31 -0400

    Ready to test

--- Gemfile
+++ Gemfile
@@ -13,11 +13,21 @@ gem 'jbuilder', '~> 2.0'
 
 # Environment Dependencies
 group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  # gem 'coveralls', require: false
   gem 'sqlite3'
   #-----------
   gem 'byebug'
   gem 'web-console', '~> 2.0'
   gem 'spring'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'faker'
+  gem 'shoulda'
 end
 
 group :production do
---
+++ spec/rails_helper.rb
@@ -0,0 +1,26 @@
+ENV["RAILS_ENV"] ||= 'test'
+require 'spec_helper'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require "capybara/rspec"
+require 'shoulda/context'
+require 'shoulda/matchers'
+#require 'devise'
+#require 'coveralls'
+#  Coveralls.wear!('rails')
+#require 'paperclip/matchers'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+
+  config.use_transactional_fixtures = false
+
+  config.infer_spec_type_from_file_location!
+
+#  config.include Paperclip::Shoulda::Matchers
+
+#  config.include Devise::TestHelpers, :type => :controller
+end
