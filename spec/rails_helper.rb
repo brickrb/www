d56af2c199541ac202a2d5cf46d80e2b175e8a5f
maclover7 <me@jonathanmoss.me>
2015-05-31 13:55:38 -0400

    Include paperclip test helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require 'shoulda/matchers'
 require 'devise'
 require 'coveralls'
   Coveralls.wear!('rails')
-# require 'paperclip/matchers'
+require 'paperclip/matchers'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
@@ -20,7 +20,7 @@ RSpec.configure do |config|
 
   config.infer_spec_type_from_file_location!
 
-#  config.include Paperclip::Shoulda::Matchers
+  config.include Paperclip::Shoulda::Matchers
 
   config.include Devise::TestHelpers, :type => :controller
 end
