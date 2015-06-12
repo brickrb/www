c8a76c1c51ae66a9d6d622e768cd67f15c115b35
maclover7 <me@jonathanmoss.me>
2015-06-12 17:47:14 -0400

    User caching updates (hopefully this fixes the problems)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ActiveRecord::Base
   has_many :oauth_applications, class_name: 'Doorkeeper::Application', as: :owner
   has_many :ownerships
   has_many :packages, through: :ownerships
+  
+  if Rails.env.production?
+    after_create :purge_all
+    after_save :purge
+    after_destroy :purge, :purge_all
+  end
 end
