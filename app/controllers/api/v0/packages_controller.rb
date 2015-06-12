1991191ec9c8b220b543e8be3bde870e0d903248
maclover7 <me@jonathanmoss.me>
2015-06-12 09:07:02 -0400

    [API] PackagesController caching!!

--- app/controllers/api/v0/packages_controller.rb
+++ app/controllers/api/v0/packages_controller.rb
@@ -1,18 +1,21 @@
 class Api::V0::PackagesController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
   before_filter :doorkeeper_authorize!
+  before_action :set_cache_control_headers, only: [:index, :show]
   before_action :set_package, only: [:show, :update, :destroy]
   before_action :valid_ownership, only: [:update, :destroy]
   respond_to :json
 
   def index
     @packages = Package.all
+    set_surrogate_key_header Package.table_key, @packages.map(&:record_key)
   end
 
   def create
     @package = Package.new(package_params)
     @ownership = Ownership.new(package_id: @package.id, user_id: current_user.id)
     if @package.save
+      @package.purge_all
       render :show, location: @package , status: 201
     else
       render json: { "error": "Package could not be saved." }, status: 422
@@ -21,6 +24,7 @@ class Api::V0::PackagesController < ApplicationController
 
   def show
     if @package
+      set_surrogate_key_header @package.record_key
     else
       render json: { "error": "Package could not be found." }, status: 404
     end
@@ -28,6 +32,7 @@ class Api::V0::PackagesController < ApplicationController
 
   def update
     if @package.update(package_params)
+      @package.purge
       render :show, location: @package , status: 200
     else
       render json: { "error": "Package could not be updated." }, status: 422
@@ -39,6 +44,8 @@ class Api::V0::PackagesController < ApplicationController
     @ownerships.destroy
 
     if @package.destroy
+      @package.purge
+      @package.purge_all
       render json: {}, status: 204
     else
       render json: { "error": "Package could not be deleted." }, status: 422
